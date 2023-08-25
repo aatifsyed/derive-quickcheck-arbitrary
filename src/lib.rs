3937a479d1322c716cd3361ebe7dd8c6233dd8fd
Aatif Syed <aatifsyedyp@gmail.com>
2023-08-25 14:22:16 +0100

    doc: library

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! Derive macro for [`quickcheck::Arbitrary`](https://docs.rs/quickcheck/latest/quickcheck/trait.Arbitrary.html).
+//!
+//! Expands to calling [`Arbitrary::arbitrary`](https://docs.rs/quickcheck/latest/quickcheck/trait.Arbitrary.html#tymethod.arbitrary)
+//! on every field of a struct.
+//!
+//! ```
+//! use derive_quickcheck_arbitrary::Arbitrary;
+//!
+//! #[derive(Clone, Arbitrary)]
+//! struct Yakshaver {
+//!     id: usize,
+//!     name: String,
+//! }
+//! ```
+//!
+//! You can customise field generation by providing a callable that accepts [`&mut quickcheck::Gen`](https://docs.rs/quickcheck/latest/quickcheck/struct.Gen.html).
+//! ```
+//! # use derive_quickcheck_arbitrary::Arbitrary;
+//! # mod num { pub fn clamp(input: usize, min: usize, max: usize) -> usize { todo!() } }
+//! #[derive(Clone, Arbitrary)]
+//! struct Yakshaver {
+//!     /// Must be less than 10_000
+//!     #[arbitrary(gen(|g| num::clamp(usize::arbitrary(g), 0, 10_000) ))]
+//!     id: usize,
+//!     name: String,
+//! }
+//! ```
+//!
+//! You can skip enum variants:
+//! ```
+//! # use derive_quickcheck_arbitrary::Arbitrary;
+//! #[derive(Clone, Arbitrary)]
+//! enum YakType {
+//!     Domestic {
+//!         name: String,
+//!     },
+//!     Wild,
+//!     #[arbitrary(skip)]
+//!     Alien,
+//! }
+//! ```
+
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::{quote, ToTokens as _};
 use structmeta::{NameArgs, StructMeta};
